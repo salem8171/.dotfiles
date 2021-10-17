36c25618882c461fec40303f062546d5e493a64a
salem8171 <mohamed-salem.krichen@tutanota.com>
2021-10-17 23:17:52 +0100

    Changed default browser to ungoogled-chromium and added wrappers for it

---
+++ .config/chromium-wrapper/config.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+CHROMIUM_ARGS=()
+CHROMIUM_ARGS+=("--password-store=basic")
+CHROMIUM_ARGS+=("--profile-directory=Default")
+CHROMIUM_ARGS+=("--test-type")
+CHROMIUM_ARGS+=("--enable-crashpad")
+CHROMIUM_ARGS+=("--remote-debugging-port=9222")
+
+CHROMIUM_ARGS+=("--flag-switches-begin")
+
+CHROMIUM_ARGS+=("--extension-mime-request-handling=always-prompt-for-install")
+CHROMIUM_ARGS+=("--extensions-on-chrome-urls")
+CHROMIUM_ARGS+=("--bookmark-bar-ntp=never")
+# CHROMIUM_ARGS+=("--close-window-with-last-tab=never")
+CHROMIUM_ARGS+=("--keep-old-history=enabled")
+CHROMIUM_ARGS+=("--disable-smooth-scrolling")
+CHROMIUM_ARGS+=("--debug-packed-apps=enabled")
+CHROMIUM_ARGS+=("--form-controls-dark-mode=enabled")
+CHROMIUM_ARGS+=("--show-avatar-button=never")
+
+CHROMIUM_FEATURES=()
+CHROMIUM_FEATURES+=("OverlayScrollbar")
+CHROMIUM_FEATURES+=("WebContentsForceDark:inversion_method/cielab_based/image_behavior/selective/text_lightness_threshold/150/background_lightness_threshold/205")
+CHROMIUM_FEATURES+=("TabHoverCardImages")
+CHROMIUM_FEATURES+=("Commander")
+
+CHROMIUM_FEATURES_STRING="${CHROMIUM_FEATURES[*]}"
+CHROMIUM_FEATURES_STRING="${CHROMIUM_FEATURES_STRING// /,}"
+CHROMIUM_ARGS+=("--enable-features=${CHROMIUM_FEATURES_STRING}")
+
+CHROMIUM_ARGS+=("--flag-switches-end")
---
+++ .config/web-browser/config.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+export SEARCH_ENGINE_QUERY='https://searx.be/search?q={}'
+# BROWSER_CMD='mimeopen {}'
+export BROWSER_CMD='gtk-launch chromium {}'
+
+# Declare the list-history function to provide history for fuzzy search
+# firefox
+# list-history() {
+#   find ~/.mozilla/firefox/ -name places.sqlite |
+#     head -1 |
+#     xargs -r -I {} cp {} /tmp/
+#   sqlite3 /tmp/places.sqlite \
+#     'SELECT DISTINCT place.url
+#     FROM moz_historyvisits as hist, moz_places as place
+#     WHERE hist.place_id == place.id
+#     ORDER BY hist.visit_date DESC;'
+# }
+
+# chromium
+list-history() {
+  cp ~/.config/chromium/Default/History /tmp/
+  sqlite3 /tmp/History \
+    'SELECT DISTINCT url, title FROM urls ORDER BY last_visit_time DESC;'
+}
